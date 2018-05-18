480e970973c6baecc4e480d7d1e897ff34e9da19
Roman Volosovskyi <roman.wolosovskyi@gmail.com>
2018-05-18 13:39:54 +0300

    confirm-message-processed for all seen messages

--- src/status_im/chat/core.cljs
+++ src/status_im/chat/core.cljs
@@ -3,16 +3,18 @@
 ;; Seen messages
 (defn receive-seen
   [chat-id sender {:keys [message-ids js-obj]} {:keys [db]}]
-  (when-let [seen-messages-ids (-> (get-in db [:chats chat-id :messages])
-                                   (select-keys message-ids)
-                                   keys)]
-    {:db (reduce
-          (fn [new-db message-id]
-            (assoc-in new-db
-                      [:chats chat-id
-                       :messages message-id
-                       :user-statuses sender]
-                      :seen))
-          db
-          seen-messages-ids)
-     :confirm-message-processed {:web3   (:web3 db) :js-obj js-obj}}))
+  (merge
+   {:confirm-message-processed {:web3   (:web3 db)
+                                :js-obj js-obj}}
+   (when-let [seen-messages-ids (-> (get-in db [:chats chat-id :messages])
+                                    (select-keys message-ids)
+                                    keys)]
+     {:db (reduce
+           (fn [new-db message-id]
+             (assoc-in new-db
+                       [:chats chat-id
+                        :messages message-id
+                        :user-statuses sender]
+                       :seen))
+           db
+           seen-messages-ids)})))
